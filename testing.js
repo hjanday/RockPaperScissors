a91f653ff6dc371d6896f397c88b28334de1c475
hjanday <jandaypreet@gmail.com>
2020-07-23 22:14:49 -0400

    added files to repo

---
+++ testing.js
@@ -0,0 +1,74 @@
+//define array and random values
+//first to 5 wins
+
+let randValue; 
+let possibleChoices = ["rock", "paper", "scissors"];
+let playerChoice;
+let computerChoice;  
+
+let userScore = 0;
+let computerScore = 0;
+
+
+
+for(let i = 0;i<20;i++) {
+function game() {
+
+
+playerChoice = prompt("rock, paper or scissors?");
+randValue = Math.floor(Math.random()*3);
+computerChoice = (possibleChoices[randValue]);
+
+
+document.getElementById("outcome").innerHTML = `You chose ${playerChoice} and computer chose ${computerChoice}`;
+document.getElementById("points").innerHTML = `User Score = ${userScore}  |   Computer Score = ${computerScore}`;
+
+if(computerChoice == "rock" && playerChoice == "scissors") {
+    computerScore++;
+    document.getElementById("points").innerHTML = `User Score = ${userScore}  |   Computer Score = ${computerScore}`;
+}
+
+else if(computerChoice == "rock" && playerChoice == "paper") {
+    userScore++;
+    document.getElementById("points").innerHTML = `User Score = ${userScore}  |   Computer Score = ${computerScore}`;
+}
+
+
+
+else if(computerChoice == "paper" && playerChoice == "scissors") {
+    userScore++;
+    document.getElementById("points").innerHTML = `User Score = ${userScore}  |   Computer Score = ${computerScore}`;
+}
+else if(computerChoice == "paper" && playerChoice == "rock") {
+    computerScore++;
+    document.getElementById("points").innerHTML = `User Score = ${userScore}  |   Computer Score = ${computerScore}`;
+}
+
+
+else if(computerChoice == "scissors" && playerChoice == "rock") {
+    userScore++;
+    document.getElementById("points").innerHTML = `User Score = ${userScore}  |   Computer Score = ${computerScore}`;
+}
+
+else if(computerChoice == "scissors" && playerChoice == "paper") {
+    computerScore++;
+    document.getElementById("points").innerHTML = `User Score = ${userScore}  |   Computer Score = ${computerScore}`;
+}
+else{
+
+}
+
+
+if(userScore == 5){
+alert("User wins!");
+
+}
+else if(computerScore == 5) {
+    alert("Computer Wins!");
+  
+}
+else{
+
+}
+}
+}
